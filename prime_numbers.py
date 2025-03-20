11eb1ebd9a43a117dbfeb9ee295f518653c489db
Mumo <janemush28@gmail.com>
2025-03-20 15:53:32 +0300

    Python Update

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,13 +1,13 @@
 def is_prime(number):
-    "check if number is prime"
+    "check if a number is prime."
     if number <= 1:
         return False
     for i in range(2, int(number**0.5) + 1):
-if number % i == 0:
-    return False
-return True
+        if number % i == 0:
+            return False
+    return True
 
 for number in range(2, 101):
     if is_prime(number):
-print(number)
+         print(number)
 
