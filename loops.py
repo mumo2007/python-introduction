851530543c177f7756236b7923dff9eca8388109
Mumo <janemush28@gmail.com>
2025-03-20 13:20:43 +0300

    Python Update

---
+++ loops.py
@@ -0,0 +1,7 @@
+#loops
+#for loop
+for number in range(1000):
+    result = number ** 2
+    if number % 2 == 0: #even
+        #if number % 2 != 0: odd
+        print(result)
---
+++ prime_numbers.py
@@ -0,0 +1,13 @@
+def is_prime(number):
+    "check if number is prime"
+    if number <= 1:
+        return False
+    for i in range(2, int(number**0.5) + 1):
+if number % i == 0:
+    return False
+return True
+
+for number in range(2, 101):
+    if is_prime(number):
+print(number)
+
